71a4bd9eb811e985744aeeb3756444aedb5c7b82
Price Kahu <iprins@yahoo.com>
2017-05-22 07:36:34 +0200

    added scroll to episode detail layout

---
+++ app/src/test/java/com/flimbis/tvmaze/tv/home/HomePresenterTest.kt
@@ -0,0 +1,16 @@
+package com.flimbis.tvmaze.tv.home
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+/**
+ * Created by Fifi on 5/22/2017.
+ */
+class HomePresenterTest {
+    @Before
+    fun setUp() {
+
+    }
+
+}
