d3cc5ab360305314ead30749fa4c24cbee7d5a68
Price Kahu <iprins@yahoo.com>
2018-06-05 00:36:58 +0200

    Updated Resource Models

--- tv-data/src/main/java/com/flimbis/tvmaze/data/model/TvMazeDataModel.kt
+++ tv-data/src/main/java/com/flimbis/tvmaze/data/model/TvMazeDataModel.kt
@@ -6,11 +6,12 @@ package com.flimbis.tvmaze.data.model
 data class Image(val medium: String, val original: String)
 
 data class Episodes(val id: Long, val name: String?, val season: Int, val number: Int,
-                    val airdate: String, val airtime: String, val runtime: Int, val image: Image, val summary: String?)
+                    val runtime: Int, val image: Image, val summary: String?)
 
-data class Schedule( val time: String, val days: List<String>)
+data class Seasons(val id: Long, val number: Int, val name: String?, val episodeOrder: Int, val premiereDate: String, val endDate: String,
+                   val image: Image, val summary: String?)
 
-data class Shows(val id: Long, val name: String, val language: String,
-                 val genres: List<String>, val status: String, val runtime: Int,
-                 val premiered: String,val schedule: Schedule, val rating: Double,
-                 val image: Image, val summary: String?)
+data class Schedule(val time: String, val days: List<String>)
+
+data class Shows(val id: Long, val url: String, val name: String, val genres: List<String>, val status: String,
+                 val schedule: Schedule, val image: Image, val summary: String?)
