39be907d837e537b82f32fbe12ae3da316e1866e
isnirp <iprins88@yahoo.com>
2018-09-01 01:43:35 +0200

    default theme setup

--- app/src/main/java/com/flimbis/tvmaze/MainActivity.kt
+++ app/src/main/java/com/flimbis/tvmaze/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.flimbis.tvmaze
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
@@ -19,11 +20,16 @@ import java.io.Serializable
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), ViewContract.View {
+    val DEF_THEME = "def"
+    val CINEMA_THEME = "cinema"
+
     @Inject
     lateinit var presenter: ShowsPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //theme
+        setTheme(getSavedTheme())
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
@@ -65,5 +71,24 @@ class MainActivity : AppCompatActivity(), ViewContract.View {
                 .appComponent(TvApplication.getInstance().getAppComponent())
                 .build()
         return component
+    }
+
+    fun saveTheme(value: String){
+        val editor: SharedPreferences.Editor = getPreferences(0).edit()
+        editor.putString("theme", value)
+        editor.commit()
+        recreate()
+    }
+
+    fun getSavedTheme(): Int {
+        val theme: String = getPreferences(0).getString("theme",DEF_THEME)
+        val value: Int
+        when(theme){
+            DEF_THEME-> value = R.style.AppTheme_Def
+            CINEMA_THEME -> value = R.style.AppTheme_Cinema
+            else -> value = R.style.AppTheme_Cinema
+        }
+
+        return value
     }
 }
--- app/src/main/java/com/flimbis/tvmaze/SplashActivity.kt
+++ app/src/main/java/com/flimbis/tvmaze/SplashActivity.kt
@@ -10,7 +10,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        /*setContentView(R.layout.activity_splash)
 
         val trd = Thread {
             try {
@@ -23,7 +23,7 @@ class SplashActivity : AppCompatActivity() {
         trd.start()
         trd.join()
 
-        startActivity<MainActivity>()
+        startActivity<MainActivity>()*/
 
     }
 
